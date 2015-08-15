2c588b699aa981d7f25d0f22d91e1f8fd43f68ea
Nao Pross <np.nexus4@gmail.com>
2015-08-15 20:49:01 +0200

    Few Changes

--- src/org/atlasengine/Window.java
+++ src/org/atlasengine/Window.java
@@ -6,15 +6,17 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
+	// Window Size
 	public int width, height;
 	// This layout allow to stack up panels on top of each other
 	private CardLayout cardLayout;
 	
 	public Window(String title, Dimension d) {
+
 		cardLayout = new CardLayout(0, 0);
 		
 		this.setTitle(title);
-		this.setSize(d.width, d.height);
+		this.setSize(d);
 		this.setLayout(cardLayout);
 		this.setVisible(true);
 	}
