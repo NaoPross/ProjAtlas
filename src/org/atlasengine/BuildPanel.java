737640b76dd024be642fefb830cc0fcc71e803b4
TheRadon <leleraffa97@hotmail.it>
2015-08-15 19:20:47 +0200

    update BuildPanel
    Per via di alcuni metodi ci conviene creare la classe sprite come classe
    figlia di BuildPanel.
    Puoi vedere dai metodi addToHier(BuildPanel panel) e da sortHier()
    Questi funzionano se sprite possiede la variabile hierarchy

--- src/org/atlasengine/BuildPanel.java
+++ src/org/atlasengine/BuildPanel.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 public class BuildPanel extends JPanel {
 	
 	public int hierarchy, objects;
+	BuildPanel[] comps;
 	public String name;
 	
 	public BuildPanel() {
@@ -18,7 +19,38 @@ public class BuildPanel extends JPanel {
 		hierarchy = 0;
 	}
 	
+	private void addToHier(BuildPanel panel) {
+		
+		BuildPanel[] comps_prov = new BuildPanel[comps.length + 1];
+		for (int i = 0; i < comps.length; i++)
+			comps_prov[i] = comps[i];
+		comps_prov[comps.length] = panel;
+		
+		comps = new BuildPanel[comps_prov.length];
+		for (int i = 0; i < comps.length; i++)
+			comps[i] = comps_prov[i];
+	}
+	
+	private void sortHier() {
+		
+		BuildPanel[] comps_prov = new BuildPanel[comps.length];
+		for (int i = 0; i < comps.length; i++)
+			comps_prov[i] = comps[i];
+		
+		comps = new BuildPanel[comps_prov.length];
+		for (int i = 0; i < comps.length; i++) {
+			for (int j = 0; j < comps.length; j++) {
+				if (comps_prov[j].hierarchy == i) {
+					comps[i] = comps_prov[i];
+				}
+			}
+		}
+	}
+	
 	public void addSprite(Sprite sprite, int x, int y) {
+		
+		addToHier(sprite);
+		sortHier();
 		
 		sprite.setLocation(x, y);
 		add(sprite);
@@ -46,6 +78,11 @@ public class BuildPanel extends JPanel {
 		
 		super.remove(comp);
 		objects --;
+	}
+	
+	public void update() {
+		
+		
 	}
 
 }
