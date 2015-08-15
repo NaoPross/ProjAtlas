730e2c6bccd4ed37fa12f9470da2eda6e9c7561c
Nao Pross <np.nexus4@gmail.com>
2015-08-15 18:09:08 +0200

    Coding Window.java

--- src/org/atlasengine/Entries.java
+++ src/org/atlasengine/Entries.java
@@ -1,5 +1,12 @@
 package org.atlasengine;
 
 public class Entries {
-
+	public String type;
+	public int life;
+	
+	public int x, y;
+	
+	public Entries() {
+		
+	}
 }
--- src/org/atlasengine/Window.java
+++ src/org/atlasengine/Window.java
@@ -1,5 +1,20 @@
 package org.atlasengine;
 
-public class Window {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Window extends JFrame {
+	public int width, height;
+	private String currentPanel;
+	
+	public Window(String title, Dimension d) {
+		this.setTitle(title);
+		this.setSize(d.width, d.height);
+	}
+
+	public void addPanel(BuildPanel p) {
+		this.add(p);
+	}
 }
