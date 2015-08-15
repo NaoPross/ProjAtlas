f9b1a73bf5ff72b369af92653167dc02a1ed30b7
Nao Pross <np.nexus4@gmail.com>
2015-08-15 19:11:46 +0200

    Adding CardLayout and WindowActionListener to switch between Panels

--- src/org/atlasengine/Window.java
+++ src/org/atlasengine/Window.java
@@ -1,21 +1,21 @@
 package org.atlasengine;
 
+import java.awt.CardLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Window extends JFrame {
 	public int width, height;
-	private String currentPanel;
+	// This layout allow to stack up panels on top of each other
+	private CardLayout cardLayout;
 	
 	public Window(String title, Dimension d) {
+		cardLayout = new CardLayout(0, 0);
+		
 		this.setTitle(title);
 		this.setSize(d.width, d.height);
+		this.setLayout(cardLayout);
 		this.setVisible(true);
-	}
-
-	public void addPanel(BuildPanel p) {
-		this.add(p);
 	}
 }
---
+++ src/org/atlasengine/WindowActionListener.java
@@ -0,0 +1,12 @@
+package org.atlasengine;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WindowActionListener implements ActionListener {
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+	
+		
+	}
+}
