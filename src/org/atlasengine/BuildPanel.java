280b3d9079b41d9730ad909830182c829a632200
TheRadon <leleraffa97@hotmail.it>
2015-08-15 18:07:29 +0200

    BuildPanel

--- src/org/atlasengine/BuildPanel.java
+++ src/org/atlasengine/BuildPanel.java
@@ -1,5 +1,51 @@
 package org.atlasengine;
 
-public class BuildPanel {
+import java.awt.Component;
+
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+public class BuildPanel extends JPanel {
+	
+	public int hierarchy, objects;
+	public String name;
+	
+	public BuildPanel() {
+		
+		setLayout(null);
+		setBorder(null);
+		super.setBackground(null);
+		hierarchy = 0;
+	}
+	
+	public void addSprite(Sprite sprite, int x, int y) {
+		
+		sprite.setLocation(x, y);
+		add(sprite);
+	}
+	
+	public void addSprite(Sprite sprite) {
+		
+		add(sprite);
+		objects ++;
+	}
+	
+	public void setBackground(Image img) {
+		
+		setBackgroundImage(img);
+		objects ++;
+	}
+	
+	public int getObjectNumber() {
+		
+		return objects;
+	}
+	
+	@Override
+	public void remove(Component comp) {
+		
+		super.remove(comp);
+		objects --;
+	}
 
 }
