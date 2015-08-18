f3763d830f1e82db55bb975cdc6eb59cd9f6e0c8
TheRadon <leleraffa97@hotmail.it>
2015-08-18 23:57:22 +0200

    Secondo compito (più difficile)
    Esercizio sugli array, condizioni e cicli for

---
+++ src/teo/compiti/Compito2.java
@@ -0,0 +1,32 @@
+package teo.compiti;
+
+public class Compito2 {
+	
+	/**
+	 * Inizializza un array
+	 * di numeri interi di 1000 slot
+	 * e compilalo con i risultati di
+	 * questa funzione matematica
+	 * 
+	 * y = 3 * x + 5 (consiglio: in un ciclo for la i prende il posto della x)
+	 * 
+	 * Dopodiche definisci un metodo che verifica
+	 * se un numero è pari o dispari
+	 * ritorna 0 se è pari
+	 * ritorna 1 se è dispari
+	 * (consiglio: l'operazione matematica resto della divisione è %)
+	 * (consiglio: utilizza il resto della divisione per 2 per verificare)
+	 * 
+	 * Applica poi il metodo a tutti i numeri dell'array
+	 * contando quanti sono i numeri pari
+	 * (consiglio: inizializza una nuova variabile intera contatore)
+	 * 
+	 * Infine stampa sulla console quanti sono
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
