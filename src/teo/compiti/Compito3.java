5efca09579af8a848422fac4bec01e401df3b978
TheRadon <leleraffa97@hotmail.it>
2015-08-20 16:37:00 +0200

    Compito3 per teo
    Introduzione al concetto di grafica

---
+++ src/teo/compiti/Compito3.java
@@ -0,0 +1,39 @@
+package teo.compiti;
+
+public class Compito3 {
+	
+	/**
+	 * Lo schermo di un pc e' gestito
+	 * come un piano ad assi cartesiani (x, y)
+	 * 
+	 * Queste due coordinate definiscono la posizione
+	 * di un pixel sullo schermo
+	 * 
+	 * Pero il punto (0, 0) dello schermo e' in alto a sinista
+	 * e piu' scendi lungo l'asse y, piu' la coordinata y aumenta
+	 * (e non diminuisce come in un grafico)
+	 * 
+	 * Crea un metodo che ha come input le coordinate
+	 * (x, y) di due punti e attraverso quelle coordinate
+	 * disegna (non graficamente ma concettualmente) un
+	 * segmento che inizia dal primo punto e finisce al secondo
+	 * 
+	 * Il metodo deve solo stampare sulla console le coordinate
+	 * (x, y) di ogni pixel che fa parte di quel segmento
+	 * in questo modo
+	 * 
+	 * Per esempio:
+	 * 0  0
+	 * 1  2
+	 * 2  4
+	 * 3  6
+	 * 4  8
+	 * ecc... fino all'ultimo punto
+	 */
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
